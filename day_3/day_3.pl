8960b683e13bce683f87d44f01e32a5a3fdab06b
Frank Hereford <frankhereford@users.noreply.github.com>
2021-12-07 18:54:25 -0600

    Make a bit mask

--- day_3/day_3.pl
+++ day_3/day_3.pl
@@ -8,4 +8,15 @@ open (my $input, '<', 'input');
 while (my $reading = <$input>) { 
   chomp $reading;
   print $reading, "\n";
+
+  my $value = oct('0b' . $reading);
+  print $value, "\n";
+
+  my $inspect_bit = 0b1;
+  for (my $bit = 0; $bit < 12; $bit++) {
+    printf("%012b\n", $inspect_bit);
+    $inspect_bit = $inspect_bit << 1;
+  }
+
+  exit;
   }
