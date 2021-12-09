84f3fa8251c6e15329d2bec5d0bcb58b8a2cc866
Frank Hereford <frank@frankhereford.com>
2021-12-09 17:53:04 -0600

    build array of bingo calls

--- day_4/day_4.pl
+++ day_4/day_4.pl
@@ -7,7 +7,13 @@ my @input = ();
 open (my $input, '<', 'test_input');
 while (my $line = <$input>) { 
   chomp $line;
+  print $line, "\n";
   push @input, $line;
 }
+
 close $input;
 
+my $bingo_calls = shift @input;
+print $bingo_calls, "\n";
+my @bingo_calls = split(/,/, $bingo_calls);
+print Dumper \@bingo_calls;
