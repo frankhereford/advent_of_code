f4322dc498887311bf476fb55b17a2589c68e247
Frank Hereford <frank@frankhereford.com>
2021-12-09 17:47:30 -0600

    read in the file

---
+++ day_4/day_4.pl
@@ -0,0 +1,12 @@
+use strict;
+use Data::Dumper;
+
+my @input = ();
+open (my $input, '<', 'test_input');
+while (my $line = <$input>) { 
+  chomp $line;
+  push @values, $line;
+}
+close $input;
+
+
