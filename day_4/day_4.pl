18e0ef75bdbb492b90a8e5abb06b26056f0b7705
Frank Hereford <frank@frankhereford.com>
2021-12-09 17:48:39 -0600

    read in the input that actually runs

--- day_4/day_4.pl
+++ day_4/day_4.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/perl
+
 use strict;
 use Data::Dumper;
 
@@ -5,8 +7,7 @@ my @input = ();
 open (my $input, '<', 'test_input');
 while (my $line = <$input>) { 
   chomp $line;
-  push @values, $line;
+  push @input, $line;
 }
 close $input;
-
 
