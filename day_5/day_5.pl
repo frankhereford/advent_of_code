86ea3139aa2b80e49005389531691dfb51a5a88d
Frank Hereford <frank@frankhereford.com>
2021-12-13 07:07:07 -0600

    Draw the map

--- day_5/day_5.pl
+++ day_5/day_5.pl
@@ -12,6 +12,8 @@ while (my $line = <$input>) {
 }
 close $input;
 
+my $floor = [];
+
 foreach my $input_line (@input) {
   $input_line =~ /(\d+),(\d+) -> (\d+),(\d+)/;
   my $line = [$1, $2, $3, $4];
@@ -23,13 +25,21 @@ foreach my $input_line (@input) {
     my $small_y = $line->[1] < $line->[3] ? $line->[1] : $line->[3];
     my $big_y   = $line->[1] > $line->[3] ? $line->[1] : $line->[3];
     print "Small Y: ", $small_y, "; Big Y: ", $big_y, "\n";
+
+    for (my $y = $small_y; $y <= $big_y; $y++) {
+      $floor->[$line->[0]]->[$y]++;
+    }
   }
 
   if ($line->[1] == $line->[3]) { # x value is the same
     my $small_x = $line->[0] < $line->[2] ? $line->[0] : $line->[2];
     my $big_x   = $line->[0] > $line->[2] ? $line->[0] : $line->[2];
     print "Small X: ", $small_x, "; Big X: ", $big_x, "\n";
+
+    for (my $x = $small_x; $x <= $big_x; $x++) {
+      $floor->[$x]->[$line->[1]]++;
+    }
   }
-
-
 }
+
+print Dumper $floor, "\n";
