7b3e4ef93bc13d5d0623b9126cb57a473438179e
Frank Hereford <frank@frankhereford.com>
2021-12-13 06:38:31 -0600

    input parser and find H/V lines

--- day_5/day_5.pl
+++ day_5/day_5.pl
@@ -4,10 +4,18 @@ use strict;
 use Data::Dumper;
 
 my @input = ();
-open (my $input, '<', 'input');
+open (my $input, '<', 'test_input');
 while (my $line = <$input>) { 
   chomp $line;
   #print $line, "\n";
   push @input, $line;
 }
 close $input;
+
+foreach my $input_line (@input) {
+  $input_line =~ /(\d+),(\d+) -> (\d+),(\d+)/;
+  my $line = [$1, $2, $3, $4];
+  next unless ($line->[0] == $line->[2] || $line->[1] == $line->[3]);
+  print $input_line, "\n";
+  #print Dumper $line;
+}
