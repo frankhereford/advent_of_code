d003f0ce88fe6e5b6bd0d517f085413c29229290
Frank Hereford <frank@frankhereford.com>
2021-12-19 10:04:19 -0500

    Capture the crab initial range

--- day_7/day_7.pl
+++ day_7/day_7.pl
@@ -13,3 +13,15 @@ close $input;
 my @crabs = split(/,/, $line);
 
 print join(",", @crabs), "\n";
+
+my $smallest_crab = undef;
+my $largest_crab = undef; 
+foreach my $crab (@crabs) {
+  $smallest_crab = $crab if $smallest_crab == undef;
+  $largest_crab = $crab if $largest_crab == undef;
+  $smallest_crab = $crab if $crab < $smallest_crab;
+  $largest_crab = $crab if $crab > $largest_crab;
+}
+
+print "Smallest Crab: ", $smallest_crab, "\n";
+print "Largest Crab: ", $largest_crab, "\n";
