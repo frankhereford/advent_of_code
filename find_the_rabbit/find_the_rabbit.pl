a49b61172a4f518cc5920439441f9c3a3ac089d3
Frank Hereford <frankhereford@users.noreply.github.com>
2021-12-21 17:21:25 -0600

    link to the puzzle

--- find_the_rabbit/find_the_rabbit.pl
+++ find_the_rabbit/find_the_rabbit.pl
@@ -3,6 +3,8 @@
 use strict;
 
 =cut
+https://youtu.be/XEt09iK8IXs?t=1266
+
 * There are 100 holes in a line, and there is a rabbit in one of the holes
 * You can only look in one hole at a time, and every time you look, the rabbit jumps to an adjacent hole
 * Better solutions does the task with the best O. 
@@ -14,5 +16,5 @@ sub find_the_rabbit {
   # the trick to this is going to be to track the even-ness of the presumed location of the rabbit, 
   # because that value will toggle back and forth on every failed peek
 
-  
+
 }
