19a3341a620b85dcf9cd67cc0f78bb2bd546906f
Frank Hereford <frankhereford@users.noreply.github.com>
2021-12-21 17:09:56 -0600

    transcribe the problem

--- find_the_rabbit/find_the_rabbit.pl
+++ find_the_rabbit/find_the_rabbit.pl
@@ -2,3 +2,10 @@
 
 use strict;
 
+=cut
+* There are 100 holes in a line, and there is a rabbit in one of the holes
+* You can only look in one hole at a time, and every time you look, the rabbit jumps to an adjacent hole
+* Better solutions does the task with the best O. 
+* Bonus points for discovering the worst case senario in terms of hole-peeks for 100 holes.
+=cut
+
