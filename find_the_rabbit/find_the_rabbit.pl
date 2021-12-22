8bff8762b226fa84fd4ab910a1a7f87fd8be33b4
Frank Hereford <frankhereford@users.noreply.github.com>
2021-12-21 21:14:00 -0600

    more differences between odd and even hole conditions?

--- find_the_rabbit/find_the_rabbit.pl
+++ find_the_rabbit/find_the_rabbit.pl
@@ -51,7 +51,14 @@ while (1) {
   if (!$number_of_peeks) { # initial guess
     # odd turn number, track from the right
     print "Initial odd turn\n";
-    $guess = $number_of_holes; # assuming we have an odd number of holes
+    if ($number_of_holes % 2) { 
+      # if the number of holes is odd ...
+      print "We have an odd number of holes to consider\n";
+      $guess = $number_of_holes - 2; # assuming we have an odd number of holes
+    } else {
+      print "We have an even number of holes to consider\n";
+      $guess = $number_of_holes - 1; 
+    }
     $last_odd_guess = $guess;
   } elsif ($number_of_peeks == 1) { 
     # initial even guess, track from the left
@@ -75,18 +82,37 @@ while (1) {
   # if non-initial odd turn and guess == -1 -- this happens first
   # if non-initial even turn and guess == number_of_holes + 1
 
-  if ($number_of_peeks && $guess == -1) {
-    # we're done with the first pass, and if we got here, our assumption about the rabbit evenness is wrong
-    print "Here is where we turn around\n";
+  if ($number_of_holes % 2) { # same tricky thing; i think it's clear that i made a mistake early on that i'm living with
+    # odd number of holes
+    if ($number_of_peeks && $guess == -1) {
+      # we're done with the first pass, and if we got here, our assumption about the rabbit evenness is wrong
+      print "Here is where we turn around\n";
 
-    if ($in_even_hole) { $in_even_hole--; } # we're not really going to use this variable are we
-    else { $in_even_hole++; }
+      if ($in_even_hole) { $in_even_hole--; } # we're not really going to use this variable are we
+      else { $in_even_hole++; }
 
-    # these become the worst named variables ever at this point
-    $last_even_guess = -1;
-    $last_odd_guess = $number_of_holes + 1;
-    next;
+      # these become the worst named variables ever at this point
+      $last_even_guess = -1;
+      $last_odd_guess = $number_of_holes + 1;
+      next;
+    }
+  } else {
+    # even number of holes
+    if ($number_of_peeks && $guess == -1) {
+      # we're done with the first pass, and if we got here, our assumption about the rabbit evenness is wrong
+      print "Here is where we turn around\n";
+
+      if ($in_even_hole) { $in_even_hole--; } # we're not really going to use this variable are we
+      else { $in_even_hole++; }
+
+      # these become the worst named variables ever at this point
+      $last_even_guess = -1;
+      $last_odd_guess = $number_of_holes + 1;
+      next;
+    }
   }
+
+    
 
   if ($number_of_peeks >= $number_of_holes * 2) {
     print "We're past \$number_of_holes * 2 ...";
