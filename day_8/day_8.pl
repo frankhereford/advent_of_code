b4cda97603d6ed79bf1690bcdce7e60e4eca9d71
Frank Hereford <frankhereford@users.noreply.github.com>
2021-12-31 10:13:39 -0600

    Ready to hand in part 1

--- day_8/day_8.pl
+++ day_8/day_8.pl
@@ -10,7 +10,33 @@ use SevenSegmentDisplay;
 my @input = ();
 open (my $input, '<', 'test_input');
 #open (my $input, '<', 'input');
-my $line = <$input>;
-chomp $line;
+while (my $line = <$input>) { 
+  chomp $line;
+  #print $line, "\n";
+  push @input, $line;
+}
 close $input;
 
+my $appearences = 0;
+
+foreach my $entry (@input) {
+  $entry =~ /(.*) \| (.*)/;
+  my $digits = $1;
+  my $outputs = $2;
+  my @outputs = split(/\s/, $outputs);
+  foreach my $output_digit (@outputs) {
+    print $output_digit, "\n";
+    my $length = length($output_digit);
+    if      ($length == 2) { # digit 1
+      $appearences++;
+    } elsif ($length == 4) { # digit 4
+      $appearences++;
+    } elsif ($length == 3) { # digit 7
+      $appearences++;
+    } elsif ($length == 7) { # digit 8
+      $appearences++;
+    }
+  }
+}
+
+print "Appearences: ", $appearences, "\n";
