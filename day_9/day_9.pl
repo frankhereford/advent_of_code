99b35f65b0c8098aa646fdb909a77449ed2e8911
Frank Hereford <frank@frankhereford.com>
2022-01-02 09:52:51 -0600

    Touch each cell

--- day_9/day_9.pl
+++ day_9/day_9.pl
@@ -15,3 +15,10 @@ while (my $line = <$input>) {
   push @input, $line;
 }
 close $input;
+
+for (my $x = 0; $x < scalar(@input); $x++) {
+  my @heights = split(//, $input[$x]);
+  for (my $y = 0; $y < scalar(@heights); $y++) {
+    #print $x, ' x ', $y, "\n";
+  }
+}
