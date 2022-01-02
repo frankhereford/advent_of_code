758f8232d8cfbbc0c63cc49d0000e2d8e946345e
Frank Hereford <frank@frankhereford.com>
2022-01-02 14:40:24 -0600

    I don't understand this puzzle yet

--- day_10/day_10.pl
+++ day_10/day_10.pl
@@ -7,11 +7,62 @@ use Data::Dumper;
 
 
 my @input = ();
-#open (my $input, '<', 'test_input');
-open (my $input, '<', 'input');
+open (my $input, '<', 'test_input');
+#open (my $input, '<', 'input');
 while (my $line = <$input>) { 
   chomp $line;
   #print $line, "\n";
   push @input, $line;
 }
 close $input;
+
+foreach my $line (@input) {
+  my $fail_char = parse_line($line);
+  print $line, "\n";
+  print "Fail char: ", $fail_char, "\n";
+  print "\n";
+}
+
+{([(<{}[<>[]}>{[]{[(<()>
+{([(<{}[[]}>{[]{[(<()>
+{([(<{}[}>{[]{[(<()>
+{([(<[}>{[]{[(<()>
+{([(<[}>{{[(<()>
+{([(<[}>{{[(<>
+{([(<[}>{{[(
+([(<[>{{[(
+([([{{[(
+
+
+sub parse_line {
+  my $line = shift;
+
+  print $line, "\n";
+
+  my %seen = {};
+
+  my @chars = split(//, $line);
+  foreach my $char (@chars) {
+    print $char, "\n";
+
+    $seen{'()'}++ if $char eq '(';
+    $seen{'[]'}++ if $char eq '[';
+    $seen{'{}'}++ if $char eq '{';
+    $seen{'<>'}++ if $char eq '<';
+
+    $seen{'()'}-- if $char eq ')';
+    $seen{'[]'}-- if $char eq ']';
+    $seen{'{}'}-- if $char eq '}';
+    $seen{'<>'}-- if $char eq '>';
+
+    return '()' if $seen{'()'} < 0;
+    return '[]' if $seen{'[]'} < 0;
+    return '{}' if $seen{'{}'} < 0;
+    return '<>' if $seen{'<>'} < 0;
+
+    print Dumper \%seen;
+    <>;
+  }
+
+  return \%seen;
+}
