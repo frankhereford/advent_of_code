fe7b82025434114b5162e65c7a0ecbdfb7135b34
Frank Hereford <frank@frankhereford.com>
2022-01-02 13:58:18 -0600

    got it

--- day_9/day_9.pl
+++ day_9/day_9.pl
@@ -7,8 +7,8 @@ use Data::Dumper;
 
 
 my @input = ();
-open (my $input, '<', 'test_input');
-#open (my $input, '<', 'input');
+#open (my $input, '<', 'test_input');
+open (my $input, '<', 'input');
 while (my $line = <$input>) { 
   chomp $line;
   #print $line, "\n";
