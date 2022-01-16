961aea42ef3fa655f8063f34970bc5abb5efbb53
Frank Hereford <frank@frankhereford.com>
2022-01-16 15:41:20 -0600

    day 14 template

---
+++ day_14/day_14.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+use FindBin;
+use lib $FindBin::Bin;
+use Term::ANSIColor;
+use Data::Dumper;
+
+my @input = ();
+open (my $input, '<', 'input');
+while (my $line = <$input>) { 
+  chomp $line;
+  #print $line, "\n";
+  push @input, $line;
+}
+close $input;
