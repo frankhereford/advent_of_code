94e16e5a47742b9a3161228a5a0ed5cf435d41a8
Frank Hereford <frank@frankhereford.com>
2022-01-15 21:22:55 -0600

    Half way to a board flashing routine

--- day_11/day_11.pl
+++ day_11/day_11.pl
@@ -27,18 +27,36 @@ for (my $x = 0; $x < 10; $x++) {
 }
 
 print_board($board);
-
-$board = increment_board_by_one($board);
 print "\n";
 
-print_board($board);
+for (my $iteration = 0; $iteration < 100; $iteration++) {
+  $board = increment_board_by_one($board);
+  $board = flash_board($board);
+  print_board($board);
+  print "\n";
+  <>;
+}
+
+
+sub flash_board {
+  my $board = shift;
+  for (my $x = 0; $x < 10; $x++) {
+    for (my $y = 0; $y < 10; $y++) {
+      if (!$board->[$x]->[$y]->{'has_flashed_this_turn'} && $board->[$x]->[$y]->{'level'} > 9) {
+        $board->[$x]->[$y]->{'has_flashed_this_turn'} = 1;
+        $board->[$x]->[$y]->{'level'} = '*';
+      }
+    }
+  }
+  return $board;
+}
 
 sub increment_board_by_one {
   my $board = shift;
 
   for (my $x = 0; $x < 10; $x++) {
     for (my $y = 0; $y < 10; $y++) {
-      $board->[$x]->[$y]->{'level'} += 1;
+      $board->[$x]->[$y]->{'level'} += 1 unless $board->[$x]->[$y]->{'level'} eq '*';
     }
   }
 
