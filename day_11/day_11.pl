3ac217363f2a2c2cd336ad402649d81f0bb35db2
Frank Hereford <frank@frankhereford.com>
2022-01-15 21:10:28 -0600

    add board printer

--- day_11/day_11.pl
+++ day_11/day_11.pl
@@ -18,11 +18,23 @@ my $board = [];
 
 for (my $x = 0; $x < 10; $x++) {
   for (my $y = 0; $y < 10; $y++) {
-    $board->[$x]->[$y] = { 
+    $board->[$y]->[$x] = { 
       level => substr($input[$y], $x, 1),
       has_flashed_this_turn => 0,
-
+    }
   }
 }
 
-print Dumper $board;
+print_board($board);
+
+#print Dumper $board;
+
+sub print_board {
+  my $board = shift;
+  for (my $x = 0; $x < 10; $x++) {
+    for (my $y = 0; $y < 10; $y++) {
+      print $board->[$x]->[$y]->{'level'};
+    }
+  print "\n";
+  }
+}
