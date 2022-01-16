8b07678dca5faa6ea2959c03b4304d4e832e577e
Frank Hereford <frank@frankhereford.com>
2022-01-16 11:35:32 -0600

    cave creator

--- day_12/day_12.pl
+++ day_12/day_12.pl
@@ -7,10 +7,23 @@ use Term::ANSIColor;
 use Data::Dumper;
 
 my @input = ();
-open (my $input, '<', 'input');
+open (my $input, '<', 'micro_test_input');
 while (my $line = <$input>) { 
   chomp $line;
   #print $line, "\n";
   push @input, $line;
 }
 close $input;
+
+my $caves = {};
+
+
+
+sub create_and_or_return_cave {
+  my $cave_name = shift;
+  return $caves->{$cave_name} if defined $caves->{$cave_name};
+  $caves->{$cave_name} = {
+    'is_large' => uc($cave_name) eq $cave_name ? 1 : 0,
+    'connections' => [],
+  };
+}
